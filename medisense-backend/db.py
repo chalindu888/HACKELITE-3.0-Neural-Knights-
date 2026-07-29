a5dba60e6675c5bfd2d812b4bf4f6cb4fcf5db83
K.R. Anushka <ravinduanushka2@gmail.com>
2026-07-29 13:35:45 +0530

    Refactor database connection to use AsyncIOMotorClient for MongoDB; add connection testing function

--- medisense-backend/db.py
+++ medisense-backend/db.py
@@ -1,19 +1,28 @@
 import os
 from dotenv import load_dotenv
-from pymongo import MongoClient
-
+from motor.motor_asyncio import AsyncIOMotorClient
 
 load_dotenv()
-
 
 MONGO_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27018")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "medisense_db")
 
-client = MongoClient(MONGO_URL)
+client = AsyncIOMotorClient(MONGO_URL)
 db = client[DATABASE_NAME]
 
-# Collections (Tables)
+# Collections
 symptoms_collection = db.get_collection("symptoms")
 diseases_collection = db.get_collection("diseases")
 patients_collection = db.get_collection("patients")
 diagnosis_records_collection = db.get_collection("diagnosis_records")
+
+
+def get_db():
+    return db
+
+async def test_connection():
+    try:
+        await client.admin.command('ping')
+        print("Connected to MongoDB successfully!")
+    except Exception as e:
+        print(f"MongoDB Connection failed: {e}")
