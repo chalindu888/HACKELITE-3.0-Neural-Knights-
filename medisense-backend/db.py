6aebce7c97c309d77f8481f6224b9dd7008a955a
K.R. Anushka <ravinduanushka2@gmail.com>
2026-07-29 06:07:19 +0530

    update

--- medisense-backend/db.py
+++ medisense-backend/db.py
@@ -1,14 +1,19 @@
+import os
+from dotenv import load_dotenv
 from pymongo import MongoClient
 
 
-MONGO_DETAILS = "mongodb://localhost:27018"
+load_dotenv()
 
-client = MongoClient(MONGO_DETAILS)
 
-db = client.medisense_db
+MONGO_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27018")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "medisense_db")
+
+client = MongoClient(MONGO_URL)
+db = client[DATABASE_NAME]
 
 # Collections (Tables)
 symptoms_collection = db.get_collection("symptoms")
 diseases_collection = db.get_collection("diseases")
 patients_collection = db.get_collection("patients")
-diagnosis_collection = db.get_collection("diagnosis_records")
+diagnosis_records_collection = db.get_collection("diagnosis_records")
